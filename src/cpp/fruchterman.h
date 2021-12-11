5db6eba96888fa303931b4c28a79d051f2d708aa
wangwei.wang <wangwei.wang@antgroup.com>
2021-12-11 11:46:35 +0800

    feat: modify fruchtermain algo

---
+++ src/cpp/fruchterman.h
@@ -0,0 +1,42 @@
+struct Node
+{
+  float x; // x coordinate
+  float y; // y coordinate
+};
+
+struct Edge
+{
+  int sourceNodeArrayIdx; // node array index
+  int targetNodeArrayIdx; // node array index
+};
+
+struct Point
+{
+  float x;
+  float y;
+};
+
+struct Center{
+  float x;
+  float y;
+};
+
+struct Cluster
+{
+  int * nodeArrayIdx;
+  int nodeSize;
+  float cx;
+  float cy;
+  int count;
+};
+
+
+void calRepulsive(const Node *nodes, const size_t nodeSize, Point *displacements, const float k2);
+
+void calAttractive(const Node *nodes, const size_t nodeSize, const Edge *edges, const size_t edgeSize, Point *displacements, const float k);
+
+void calCluster(const Node *nodes, const size_t nodeSize, const Cluster *clusters, const size_t clusterSize, Point *displacements, const float clusterGravity, const float k);
+
+void calGravity(Node *nodes, size_t nodeSize, Point *displacements, float gravity, float k, Center *center);
+
+// void reCalNodesPosition(Node *nodes, size_t nodeSize, Point *displacements, unsigned int clusterSize, float speed);
